78d1edf20e57ebd3a3fac7346efe3275bbac8d36
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-02-12 19:30:21 -0700

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length === 1) {
+        return a[0]; }
+
+    let n = a.length;
+    let div = math.floor(n/3);
+    let subA1 = a.slice(0, div);
+    let subA2 = a.slice(0, 2 * div);
+    let subA3 = a.slice(o, 3 * div);
+
+    let sum1 = divideAndConquerSum(subA1);
+    let sum2 = divideAndConquerSum(subA2);
+    let sum3 = divideAndConquerSum(subA2);
+    
+    return sum1 + sum2 + sum3;
 }
